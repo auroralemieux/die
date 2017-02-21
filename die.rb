b1791b3262eea9a8033ff809c5415ba31208b2f1
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-02-21 11:21:33 -0800

    added tests for less than 7 and more than 0

--- die.rb
+++ die.rb
@@ -1,1 +1,10 @@
 # die.rb
+
+class Die
+  attr_reader :showing_number
+  def initialize
+    @showing_number = rand(1..6)
+  end
+  # def showing_number
+  # end
+end
--- die_test.rb
+++ die_test.rb
@@ -4,3 +4,23 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 
 require_relative 'die'
+
+
+class TestDie < Minitest::Unit::TestCase
+
+  def test_creation_of_die
+    die = Die.new
+    assert die.class == Die, "A Die cannot be created as an instance of Die"
+  end
+
+  def test_showing_number_must_be_less_than_7
+    die = Die.new
+    assert die.showing_number < 7
+  end
+
+  def test_showing_number_must_be_greater_than_0
+    die = Die.new
+    assert die.showing_number > 0
+  end
+
+end
